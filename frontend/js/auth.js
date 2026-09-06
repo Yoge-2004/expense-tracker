ccb23431b8694e70b999549b15cd4852df7a6a3a
Yogeshwaran M <yoge852004@gmail.com>
2026-09-06 11:12:45 +0530

    Store email for Google web sessions

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -51,7 +51,7 @@ function setGoogleButtonLoading(loading, message = "Connecting to Google...") {
         } else {
             btn.classList.remove("is-loading");
             btn.innerHTML = `
-                <svg width="18" height="18" viewBox="0 0 24 24"><path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/><path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/><path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/><path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"/></svg>
+                <svg width="18" height="18" viewBox="0 0 24 24"><path fill="#4285F4" d="M22.56 12.25c-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/><path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84c1.81 3.59 5.52 6.06 9.82 6.06z"/><path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/><path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"/></svg>
                 <span>Continue with Google</span>
             `;
         }
@@ -106,6 +106,7 @@ async function handleGoogleCredentialResponse(credentialResponse) {
         localStorage.setItem("token", response.token);
         localStorage.setItem("userId", response.userId);
         localStorage.setItem("userName", response.name || "Google User");
+        localStorage.setItem("userEmail", response.email || credentialResponse.email || "");
         showToast("Signed in with Google! Redirecting...", "success");
         setTimeout(() => { window.location.href = "dashboard.html"; }, 500);
     } catch (error) {
@@ -142,7 +143,7 @@ function handleGoogleOAuth() {
                 setTimeout(() => {
                     setGoogleButtonLoading(false);
                     if (reason === "opt_out_or_no_session") {
-                        showToast("Please select your Google Account or sign in using the Google button.", "info");
+                        showToast("Please select your Google Account or sign in with the Google button.", "info");
                     } else if (reason === "suppressed_by_user") {
                         showToast("Google prompt was dismissed recently. Please click the Google button to sign in.", "info");
                     } else {
