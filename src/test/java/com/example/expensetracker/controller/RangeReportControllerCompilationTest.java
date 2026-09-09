04c63c719e0706f33766fe617239c9eac33474dd
Yogeshwaran M <yoge852004@gmail.com>
2026-09-09 15:17:23 +0530

    test(reports): verify range export controller is part of test build

---
+++ src/test/java/com/example/expensetracker/controller/RangeReportControllerCompilationTest.java
@@ -0,0 +1,12 @@
+package com.example.expensetracker.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class RangeReportControllerCompilationTest {
+    @Test
+    void rangeReportControllerIsPresentAfterBuildRefactor() {
+        assertNotNull(RangeReportController.class);
+    }
+}
